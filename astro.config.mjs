ac07cbbe6475cadee980b4c57fbd847a19dfdf82
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2023-12-23 00:47:58 +0000

    Public Version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(
-		{
-			extendMarkdownConfig: false,
-			smartypants: true,
-			gfm: true,
-		}
-	), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx({
+    extendMarkdownConfig: false,
+    smartypants: true,
+    gfm: true
+  }), sitemap(), tailwind()]
 });
