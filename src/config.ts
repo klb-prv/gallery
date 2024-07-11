4f2f3e5d54962c88c6c19394d8722ec16689642a
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-07-12 00:41:34 +0200

    LVL Up to version v.1.2

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'KB Gallery';
+export const SITE_DESCRIPTION = 'Enjoy my projects';
 declare module '*.png'
 
